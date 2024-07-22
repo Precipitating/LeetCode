52ae9342edbeadb2313852fefcde0ea7e493a367
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-07-23 00:08:47 +0100

    Added palindrome soluton 1, my own solution.

---
+++ Palindrome Number/Source.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    // solution 1
+    //bool isPalindrome(int x)
+    //{
+    //    string toStr = to_string(x);
+    //    string reversed = { toStr.rbegin(), toStr.rend() };
+
+    //    return (toStr == reversed);
+    //}
+
+
+};
+
+
+int main()
+{
+    Solution s;
+    int num = 121;
+    cout << s.isPalindrome(num);
+
+    return 0;
+}
