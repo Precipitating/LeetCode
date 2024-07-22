9fa99a2858801dd1d1f5dcc9bc1757265668d188
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-07-22 04:06:34 +0100

    Added my solution. Checks ahead and subtracts accordingly if its a higher value than current numeral.

--- Roman To Integer/Source.cpp
+++ Roman To Integer/Source.cpp
@@ -1,18 +1,55 @@
 #include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cmath> 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int romanToInt(string s) {
+    int romanToInt(string s)
+    {
+        unordered_map<char, int> romanMap = { {'I', 1}, {'V', 5}, {'X', 10}, {'L', 50}, {'C', 100}, {'D', 500}, {'M', 1000} };
+        int answer = 0;
+
+        for (size_t i = 0; i < s.size(); ++i)
+        {
+            char currentNumeral = s[i];
+            // if we arrive at last symbol, dont bother checking ahead and finish up.
+            if (i == s.size() - 1)
+            {
+                answer += romanMap[currentNumeral];
+                break;
+            }
+
+            // if next char is bigger, subtract both values and ignore next char.
+            if (romanMap[s[i + 1]] > romanMap[currentNumeral] )
+            {
+                answer += romanMap[s[i + 1]] - romanMap[currentNumeral]; 
+                ++i;
+            }
+            else
+            {
+                answer += romanMap[currentNumeral];
+            }
+
+            
+        }
+
+        return answer;
 
     }
-};
 
+
+
+};
 
 int main()
 {
     Solution sol;
+    string romanNumeral = "IV";
+    cout << sol.romanToInt(romanNumeral);
 
-    sol.romanToInt();
     return 0;
 }
+
