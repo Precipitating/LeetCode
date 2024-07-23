8eebce1fe625cd8e54ddbb52fbcbb983b50a1624
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-07-23 03:47:38 +0100

    Add solution 2, no string conversion

--- Palindrome Number/Source.cpp
+++ Palindrome Number/Source.cpp
@@ -14,7 +14,30 @@ public:
     //    return (toStr == reversed);
     //}
 
+    // solution 2, no string conversion
+    bool isPalindrome(int x)
+    {
+        // it's not a palindrome if its less than 0
+        if (x < 0)
+        {
+            return false;
+        }
 
+        long long reversed = 0;
+        long long tempX = x;
+        while (tempX != 0)
+        {
+            // extract last digit
+            long long lastDig = tempX % 10;
+            //multiply by 10 to add 0 to the end, then add the last digit to emulate concatenation
+            reversed = (reversed * 10) + lastDig;
+            // remove last digit
+            tempX /= 10;
+        }
+
+        return x == reversed;
+
+    }
 };
 
 
