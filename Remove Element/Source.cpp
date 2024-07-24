de82ef77719995b7ec72e58333ac2244a187cc03
Eason Lee <eason-lee@hotmail.co.uk>
2024-07-24 04:13:36 +0100

    Added new method, moving all targets to end and resizing

--- Remove Element/Source.cpp
+++ Remove Element/Source.cpp
@@ -8,9 +8,21 @@ class Solution {
 public:
 
     // soluton 1, simple erase remove idiom, not that efficient.
-    int removeElement(vector<int>& nums, int val) 
+    //int removeElement(vector<int>& nums, int val) 
+    //{
+    //    nums.erase(remove(nums.begin(), nums.end(), val),nums.end());
+
+    //    return nums.size();
+
+    //}
+
+
+    // solution 2, move all val numbers to the end of the vector, no erasure.
+    int removeElement(vector<int>& nums, int val)
     {
-        nums.erase(remove(nums.begin(), nums.end(), val),nums.end());
+        auto it = std::partition(nums.begin(), nums.end(), [val](int num) {return num != val; });
+
+        nums.resize(distance(nums.begin(), it));
 
         return nums.size();
 
@@ -24,7 +36,9 @@ int main()
     vector<int> nums = { 0,1,2,2,3,0,4,2 };
     int val = 2;
     int k = s.removeElement(nums, val);
-    assert(k == nums.size());
+
+
+
     sort(nums.begin(), nums.begin() + k);
 
 
