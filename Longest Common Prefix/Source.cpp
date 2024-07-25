6ddd334f68133272f8861253b32e5e4ce5bcd19e
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-07-25 03:05:06 +0100

    comments.

--- Longest Common Prefix/Source.cpp
+++ Longest Common Prefix/Source.cpp
@@ -8,11 +8,14 @@ class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs)
     {
+
+        // its the only possible prefix if its 1
         if (strs.size() == 1)
         {
             return strs.front();
         }
 
+        // if every word is same, return first element string
         if (adjacent_find(strs.begin(), strs.end(), not_equal_to<>()) == strs.end())
         {
             return strs.front();
@@ -20,32 +23,41 @@ public:
 
 
 
-
+        // current substring len
         int length = 1;
         for (size_t i = 0; i < strs[0].size(); ++i)
         {
+            // if current prefix matches all other other strings, increase length.
             if (FullMatch(strs, length))
             {
+                // if its same size as first string, we cant go further so it must be the max prefix.
                 if (length == strs[0].size())
                 {
                     return strs[0].substr(0, length);
                 }
+
                 ++length;
 
             }
             else
             {
+                // if it's 1, then the 2nd element in vector does not match the first at all,
+                //  making the whole thing invalid as there must be a common prefix for all elements.
                 if (length == 1)
                 {
                     return "";
                 }
 
+                // return the max prefix.
                 return strs[0].substr(0, length - 1);
             }
         }
 
     }
 
+    // compare the first string substring with all the other strings
+    // return true if every other string matches current substring
+    // else one string doesn't match the current prefix,  
     bool FullMatch(const vector<string>& strs, int& length)
     {
         bool result = false;
@@ -84,7 +96,7 @@ public:
 
 int main()
 {
-    vector<string> words = { "abb","abc" };
+    vector<string> words = { "ab","abcdefg" };
     Solution s;
     cout << s.longestCommonPrefix(words);
 
