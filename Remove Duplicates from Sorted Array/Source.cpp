9e82244bfcbfde2e72a0ff6ddcf6e6956576d104
Eason Lee <eason-lee@hotmail.co.uk>
2024-07-28 00:02:14 +0100

    Solution 2 added

--- Remove Duplicates from Sorted Array/Source.cpp
+++ Remove Duplicates from Sorted Array/Source.cpp
@@ -10,15 +10,38 @@ class Solution
 {
 public:
     // solution 1, using STL
+    //int removeDuplicates(vector<int>& nums)
+    //{
+    //    int same = 0;
+
+    //    auto last = unique(nums.begin(), nums.end());
+
+
+
+    //    return std::distance(nums.begin(), last);
+
+    //}
+
+    // sol 2, faster
+    // check prev, and if not same number it's not ordered.
     int removeDuplicates(vector<int>& nums)
     {
-        int same = 0;
+        int unique = 1;
+        int j = 1;
 
-        auto last = unique(nums.begin(), nums.end());
+        for (size_t i = 1; i < nums.size(); ++i)
+        {
+            if (nums[i] != nums[i - 1])
+            {
+                nums[j] = nums[i];
+                ++j;
+                ++unique;
+            }
+        }
 
 
+        return unique;
 
-        return std::distance(nums.begin(), last);
 
     }
 };
@@ -26,7 +49,7 @@ public:
 int main()
 {
     Solution s;
-    vector<int> nums = { 0,0,1,1,1,2,2,3,3,4 };
+    vector<int> nums = { 1,1,2,3,3,4};
     int k = s.removeDuplicates(nums);
 
 	return 0;
