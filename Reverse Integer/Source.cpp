fe26677fe0ed52108eb6cb587a98dc2052c35c6f
Eason Lee <eason-lee@hotmail.co.uk>
2024-08-04 02:23:35 +0100

    Reverse integer solution: O(Log(X))

---
+++ Reverse Integer/Source.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+
+class Solution
+{
+public:
+    int reverse(int x)
+    {
+        bool isNeg = false;
+        int result = 0;
+
+
+        while (x != 0)
+        {
+            if ( result > INT_MAX / 10 || result < INT_MIN / 10)
+            {
+                return 0;
+            }
+            result *= 10;
+            result += x % 10;
+            x /= 10;
+
+
+
+        }
+
+
+
+
+        if (isNeg)
+        {
+            result *= -1;
+        }
+        return result;
+
+
+       
+    }
+};
+
+
+int main()
+{
+    int x = -2147483412;
+
+    Solution s;
+
+    cout << s.reverse(x);
+
+
+
+
+    return 0;
+}
