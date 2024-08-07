2b77c1de707594ea1a7dbf001769a1f0937e45c7
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-08-07 04:46:32 +0100

    Added top down memoization method.

---
+++ Regular Expression Matching/Source.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+
+class Solution
+{
+public:
+    // Top down memoization
+    bool isMatch(string s, string p)
+    {
+
+        return dfs(0, 0, s, p);
+
+
+    }
+
+    // depth first search
+    bool dfs(int i, int j, const string& s, const string& p)
+    {
+        if (cache.count(make_pair(i, j)) == 1)
+        {
+            return cache[make_pair(i, j)];
+        }
+
+        // edge cases
+        if (i >= s.length() && j >= p.length()) { return true; }
+        if (j >= p.length()) { return false; }
+
+        // if s in bounds and char is . or equal..
+        bool match = (i < s.length()) && (s[i] == p[j] || (p[j] == '.'));
+
+        // if current pattern char equals * & in bounds
+        if ((j + 1) < p.length() && p[j + 1] == '*')
+        {
+            cache[make_pair(i,j)] = (dfs(i, j + 2, s, p) || (match && dfs(i + 1, j, s, p)));
+            return cache[make_pair(i, j)];
+
+        }
+        if (match)
+        {
+            cache[make_pair(i, j)] = dfs(i + 1, j + 1, s, p);
+            return cache[make_pair(i, j)];
+        }
+        cache[make_pair(i, j)] = false;
+        return cache[make_pair(i, j)];
+
+    }
+
+private:
+    map<pair<int, int>, bool> cache;
+
+
+};
+
+
+
+int main()
+{
+    Solution s;
+    cout << s.isMatch("ab", ".*c");
+    return 0;
+}
