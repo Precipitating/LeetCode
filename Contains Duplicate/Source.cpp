40b90bbd62b6c29fb159e834f9c35b0cacbd8e2b
Eason Lee <eason-lee@hotmail.co.uk>
2024-08-17 22:46:54 +0100

    Added O(n log n) solution

--- Contains Duplicate/Source.cpp
+++ Contains Duplicate/Source.cpp
@@ -1,38 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <unordered_set>
+#include <algorithm>
 using namespace std;
 
 class Solution
 {
 public:
     // space & time complexity = o(N)
+    //bool containsDuplicate(vector<int>& nums)
+    //{
+    //    bool result = false;
+    //    unordered_set<int> seenValues;
+    //    for (size_t i = 0; i < nums.size(); ++i)
+    //    {
+    //        if (seenValues.count(nums[i]) != 0)
+    //        {
+    //            result = true;
+    //            return result;
+    //        }
+    //        else
+    //        {
+    //            seenValues.insert(nums[i]);
+    //        }
+    //    }
+    //    return result;
+    //}
+    
+
+
+    // o(n log n), sort and check prev. O(1) space complexity
     bool containsDuplicate(vector<int>& nums)
     {
-        bool result = false;
-        unordered_set<int> seenValues;
+        std::sort(nums.begin(), nums.end());
 
-        for (size_t i = 0; i < nums.size(); ++i)
+        for (size_t i = 1; i < nums.size(); ++i)
         {
-            if (seenValues.count(nums[i]) != 0)
+            if (nums[i] == nums[i - 1])
             {
-                result = true;
-                return result;
-            }
-            else
-            {
-                seenValues.insert(nums[i]);
+                return true;
             }
         }
 
-        return result;
+        return false;
     }
 };
 
 int main()
 {
     Solution s;
-    vector<int> list = { 1,2,3,4,5 };
+    vector<int> list = { 1,2,2,3,4,5 };
     cout << s.containsDuplicate(list);
     return 0;
 }
