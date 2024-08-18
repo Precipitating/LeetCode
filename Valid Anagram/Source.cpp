c79a230655e9a0231560fd30aaa2afbe3a50c57c
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-08-18 21:20:52 +0100

    Valid Anagram added O(n) runtime, O(1) space complexity.

---
+++ Valid Anagram/Source.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    // using hash map O(n) runtime, O(1) space complexity.
+    bool isAnagram(string s, string t)
+    {
+        bool result;
+        unordered_map<char,int> map;
+        unordered_map<char,int> map2;
+
+        if (s.length() != t.length())
+        {
+            return false;
+        }
+
+        for (const char& c : s)
+        {
+            map[c] += 1;
+        }
+
+        for (const char& c : t)
+        {
+            map2[c] += 1;
+        }
+
+        result = (map == map2) ? true : false;
+
+        return result;
+
+    }
+};
+
+
+int main()
+{
+    Solution s;
+    cout << s.isAnagram("anagram", "nagaram");
+    return 0;
+}
