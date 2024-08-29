2df139217c55b2a9ddbb27ce52b7125132c26b82
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-08-29 21:38:19 +0100

    Added Heaps algorithm o(N!)

---
+++ Permutations/Source.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<vector<int>> permute(vector<int>& nums)
+    {
+        if (nums.size() == 1) { return { {nums[0]} }; }
+
+        vector<vector<int>> result;
+
+        RecursivePermutation(nums.size(), nums, result);
+
+        return result;
+    }
+
+    void RecursivePermutation(int idx,vector<int>& nums, vector<vector<int>>& result)
+    {
+        if (idx == 1) { result.push_back(nums); }
+        else
+        {
+            RecursivePermutation(idx - 1, nums, result);
+            for (int i = 0; i < idx - 1; ++i)
+            {
+                if (idx % 2 == 0)
+                {
+                    swap(nums[i], nums[idx-1]);
+
+                }
+                else
+                {
+                    swap(nums[0], nums[idx-1]);
+                }
+                RecursivePermutation(idx-1, nums, result);
+            }
+
+
+            
+        }
+
+
+
+
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> nums = { 0,1 };
+    s.permute(nums);
+}
