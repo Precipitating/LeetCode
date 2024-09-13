68ea203ddaf33bc0c457306083ef418bfda46ae8
Precipitating <92101891+Precipitating@users.noreply.github.com>
2024-09-13 21:11:21 +0100

    Simplified the solution

--- Find Peak Element/Source.cpp
+++ Find Peak Element/Source.cpp
@@ -8,66 +8,27 @@ class Solution
 public:
     int findPeakElement(vector<int>& nums)
     {
-        if (nums.size() == 1) { return 0; }
-        int result = -1;
         int low = 0, high = nums.size() - 1;
 
-
-        while (low <= high && result == -1)
+        while (low < high)
         {
-            int half = low + ((high - low) / 2);
+            int mid = low + (high - low) / 2;
 
-            // out of bounds = -INF
-            if (half + 1 == nums.size() || half - 1 < 0)
+            // if middle less than next element, move right
+            if (nums[mid] < nums[mid + 1])
             {
-                if (half + 1 == nums.size())
-                {
-                    if (nums[half - 1] < nums[half])
-                    {
-                        result = half;
-                    }
-                    else
-                    {
-                        --high;
-                    }
-                }
-                else if (half - 1 < 0)
-                {
-                    if (nums[half + 1] < nums[half])
-                    {
-                        result = half;
-                    }
-                    else
-                    {
-                        ++low;
-                    }
-                }
+                low = mid + 1;
             }
             else
             {
-                // if middle is the peak, return it.
-                if (nums[half - 1] < nums[half] && nums[half + 1] < nums[half])
-                {
-                    result = half;
-                }
-                else
-                {
-                    if (nums[half + 1] > nums[half])
-                    {
-                        low = half + 1;
-                    }
-                    else
-                    {
-                        high = half - 1;
-                    }
-                }
-
-
-
+                // else peak is left or itself is the peak.
+                high = mid;
             }
 
         }
-        return result;
+
+        return low;
+
     }
 
 
@@ -77,6 +38,6 @@ int main()
 {
     Solution s;
     vector<int> nums = { 1,2,3,1,100,200,300,400,500,600,700 };
-    s.findPeakElement(nums);
+    cout << s.findPeakElement(nums);
     return 0;
 }
