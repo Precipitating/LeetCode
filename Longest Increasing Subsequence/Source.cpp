95b229ea562e21bb0b9c84f11e932ac625389a96
Eason Lee <eason-lee@hotmail.co.uk>
2024-09-24 00:43:45 +0100

    Added o(n log n) solution

--- Longest Increasing Subsequence/Source.cpp
+++ Longest Increasing Subsequence/Source.cpp
@@ -8,21 +8,39 @@ class Solution
 public:
     int lengthOfLIS(vector<int>& nums)
     {
-        // DP O(n^2) solution
-        vector<int> dp(nums.size(),1);
+        //    // DP O(n^2) solution
+        //    vector<int> dp(nums.size(),1);
+        //    for (int i = nums.size() - 1; i > -1; --i)
+        //    {
+        //        for (int j = i+1; j < nums.size(); ++j)
+        //        {
+        //            if (nums[i] < nums[j])
+        //            {
+        //                dp[i] = max(dp[i], dp[j] + 1);
+        //            }
+        //        }
+        //    }
+        //    return *max_element(dp.begin(), dp.end());
+        //}
 
-        for (int i = nums.size() - 1; i > -1; --i)
+
+        // O (n log n) solution using build in algorithms
+        vector<int> lowerNums;
+        for (const int& num : nums)
         {
-            for (int j = i+1; j < nums.size(); ++j)
+            auto lowNum = lower_bound(lowerNums.begin(), lowerNums.end(), num);
+            if (lowNum == lowerNums.end())
             {
-                if (nums[i] < nums[j])
-                {
-                    dp[i] = max(dp[i], dp[j] + 1);
-                }
+                lowerNums.push_back(num);
             }
+            else
+            {
+                *lowNum = num;
+            }
+
         }
 
-        return *max_element(dp.begin(), dp.end());
+        return lowerNums.size();
     }
 
 };
