a7ed6b4dd21634ff2e53e2dbc7f8b35027f370bd
Eason Lee <eason-lee@hotmail.co.uk>
2024-09-24 00:25:58 +0100

    Fixed o(n^2) solution

--- Longest Increasing Subsequence/Source.cpp
+++ Longest Increasing Subsequence/Source.cpp
@@ -8,9 +8,10 @@ class Solution
 public:
     int lengthOfLIS(vector<int>& nums)
     {
+        // DP O(n^2) solution
         vector<int> dp(nums.size(),1);
 
-        for (int i = nums.size() - 1; i > 0; --i)
+        for (int i = nums.size() - 1; i > -1; --i)
         {
             for (int j = i+1; j < nums.size(); ++j)
             {
@@ -29,7 +30,7 @@ public:
 int main()
 {
     Solution s;
-    vector<int> nums = { 10,9,2,5,3,7,101,18 };
+    vector<int> nums = { 0,1,0,3,2,3 };
     cout << s.lengthOfLIS(nums);
 
     return 0;
